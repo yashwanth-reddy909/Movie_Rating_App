dd00d02eb5f3a71d27b371df878ccd0c8878b16b
Yashwanth Reddy <yaswanthreddy909@gmail.com>
2022-12-03 11:24:52 +0530

    Password ret removed3

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'pas': '2risktosay',
-    'OAuth1': '72127773199-amelar8j5711ejjeu68vvjmd6r5qjsm3.apps.googleusercontent.com',
-    'OAuth2': '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',
     'refreshToken': '1//04TnvobtmBVUzCgYIARAAGAQSNwF-L9IrKSPcxBmLbcE0FGDHLIbu8Oe2hdZxquP2HnodweiJuCHrPi75LzHH83bP9ZzVhmJLR2c'
 }
